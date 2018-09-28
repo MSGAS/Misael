4d332071648ff07f386491c7ac9b4221608c473a
Misael Govea <msgas2_@outlook.com>
2018-09-28 02:12:18 -0500

    Primer Commit GitKraken

---
+++ CrabWorld.java
@@ -0,0 +1,13 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class CrabWorld extends greenfoot.World
+{
+
+    
+    public CrabWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(560, 560, 1); 
+    }
+}
